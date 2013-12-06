e9942680f983367227d0c140c03fda0ce228d7b3
Alexander Makarov <Alexander.Makarov@trs-it.ru>
2013-12-06 16:33:07 +0400

    clean up

--- server/src/main/java/server/TestEjb.java
+++ server/src/main/java/server/TestEjb.java
@@ -24,21 +24,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TestEjb implements TestRemote {
     private static AtomicInteger count = new AtomicInteger();
     private static AtomicInteger max = new AtomicInteger();
-    private static Map<String, String> threads = new ConcurrentHashMap<>();
 
     @Override
     public String foo(String bar) {
         int cur = count.incrementAndGet();
-        threads.put(Thread.currentThread().getName(), "");
         int maxVal = max.get();
         while (cur > maxVal && !max.compareAndSet(maxVal, cur)) {
             maxVal = max.get();
         }
-//        for (int i = 0; i < 1; ++i) {
-//            System.out.println("ZzzzzzXXX...");
-//            ThreadUtils.sleep(1000);
-//        }
         count.decrementAndGet();
-        return bar.toUpperCase() + "XXX " + System.getProperty("jboss.node.name") + "\n";
+        return bar + " " + max + "\n";
     }
+
 }
